c9bb15279b9a1f95699544057c5fa3455f2ba3ad
raiesbo <raiesbo8@gmail.com>
2020-11-05 15:28:26 +0100

    img dowloader chrome extension finished

---
+++ imagesDownloader_js/background.js
@@ -0,0 +1,8 @@
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.action === "download") {
+        for(let i = 0; i < request.data.length; i++){
+            chrome.downloads.download({"url": request.data[i].src});
+        }
+        sendResponse("Done")
+    }
+})

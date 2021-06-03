5a31bd3510ec6fce3b2f19f57fa14f6bb5dd652b
raiesbo <raiesbo8@gmail.com>
2021-06-03 14:04:57 +0200

    stack definition

--- dataStructuresAndAlgos/stack.js
+++ dataStructuresAndAlgos/stack.js
@@ -2,6 +2,27 @@ class Stack {
     constructor(arr = []) {
         this.list = arr
     }
+
+    push = element => {
+        this.list.push(element)
+    }
+
+    pop = () => {
+        return this.list.pop()
+    }
+
+    clear = () => {
+        this.list = []
+    }
+
+    size = () => {
+        return this.list.length
+    }
+
+    print = () => {
+        console.log(this.list.toString)
+    }
+
 }
 
 
--- dataStructuresAndAlgos/stack.test.js
+++ dataStructuresAndAlgos/stack.test.js
@@ -1,5 +1,29 @@
 const Stack = require('./stack');
 
-test('', () => {
-    expect("").toEqual("")
+test('instanciates a class with and without default list elementos', () => {
+    const newStack = new Stack([1, 2, 3])
+    expect(newStack.list).toEqual([1, 2, 3])
+    const newStack2 = new Stack()
+    expect(newStack2.list).toEqual([])
+})
+
+test("it is possible to add element at the top of the stack", () => {
+    const newStack = new Stack()
+    newStack.push(1)
+    newStack.push(2)
+    expect(newStack.list).toEqual([1, 2])
+})
+
+test("it is possible to pop the last element", () => {
+    const newStack = new Stack()
+    newStack.push(1)
+    newStack.push(2)
+    expect(newStack.pop()).toEqual(2)
+    expect(newStack.list).toEqual([1])
+})
+
+test('clear method empties the list', () => {
+    const newStack = new Stack([1, 2, 3])
+    newStack.clear()
+    expect(newStack.list).toEqual([])
 })

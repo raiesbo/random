c80d1f31cdb96f772b12bc7c08ea331e55f7fe63
raiesbo <raiesbo8@gmail.com>
2021-06-06 00:19:32 +0200

    Queue structure

--- dataStructuresAndAlgos/queue.js
+++ dataStructuresAndAlgos/queue.js
@@ -2,6 +2,19 @@ class Queue {
     constructor(arr = []) {
         this.list = arr;
     }
+    enqueue = element => {
+        this.list.push(element);
+    }
+    dequeue = () => {
+        this.list.shift();
+    }
+    clear = () => {
+        this.list = []
+    }
+    print = () => {
+        console.log(this.list.toString)
+        return this.list.toString
+    }
 }
 
 module.exports = Queue;

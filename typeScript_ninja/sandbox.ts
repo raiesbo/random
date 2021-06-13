d69e09af12c2c76e216565d8fa504e44d383da48
raiesbo <raiesbo8@gmail.com>
2021-06-13 23:01:27 +0200

    typescript tutorial first steps

---
+++ typeScript_ninja/sandbox.ts
@@ -0,0 +1,12 @@
+const character :string = "mario";
+
+
+console.log(character)
+
+const inputs = document.querySelectorAll('input');
+
+console.log(inputs)
+
+inputs.forEach(input => {
+    console.log(input)
+})

1e084cfd9050759ac4af7bce7b78b8c64124910a
raiesbo <raiesbo8@gmail.com>
2021-06-24 07:46:53 +0200

    tsconfig settings, bundle generator, map

--- ts_webpack_ninja/webpack.config.js
+++ ts_webpack_ninja/webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 
 module.exports = {
+    mode: 'development',
+    devtool: 'eval-source-map',
     entry: './src/index.ts',
     module: {
         rules: [
@@ -11,7 +13,11 @@ module.exports = {
             }
         ]
     },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
     output: {
+        publicPath: 'public',
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'public')
     }

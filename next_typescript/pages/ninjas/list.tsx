fec54ece1b96c127c5cdb459825d45907b364473
raiesbo <raiesbo8@gmail.com>
2021-06-27 12:47:22 +0200

    fetching data, dinamic routes

--- next_typescript/pages/ninjas/list.tsx
+++ next_typescript/pages/ninjas/list.tsx
@@ -1,15 +1,41 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+import styles from '../../styles/Ninjas.module.css'
 
-export default function List() {
+export const getStaticProps = async () => {
+  const response: Response = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data: object = await response.json();
+
+  return {
+    props: { ninjas: data }
+  }
+}
+
+interface User {
+  id: number,
+  name: string
+}
+
+export default function List( props: any ) {
+
   return (
     <>
     <Head>
-      <title>Homepage</title>
+      <title>Ninjas List</title>
       <meta name="keywords" content="ninjas list" />
     </Head>
     <div>
         <h1>List</h1>
+        {
+          props.ninjas.map((ninja: User) => {
+            return (
+              <Link href={`/ninjas/${ninja.id}`} key={ ninja.id }>
+                <a className={styles.single}>
+                  <h3>{ ninja.name }</h3>
+                </a>
+              </Link>
+            )
+          })}
     </div>
     </>
   )

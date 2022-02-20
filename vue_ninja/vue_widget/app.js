883d8d259a384d1723df728a678d9c06a7398fad
raiesbo <raiesbo@gmail.com>
2022-02-20 17:07:55 +0100

    Further Vue elements: directives, computed and data binding

--- vue_ninja/vue_widget/app.js
+++ vue_ninja/vue_widget/app.js
@@ -8,7 +8,15 @@ const app = Vue.createApp({
             showBooks: true,
             title: "The Final Empire",
             author: "Brandon Sanderson",
-            age: 45
+            age: 45,
+            x: 0,
+            y: 0,
+            books: [
+                {title: 'name of the wind', author: 'patrick rothfuss', img: 'assets/1.jpeg', isFav: true},
+                {title: 'the way of kings', author: 'brandon sanderson', img: 'assets/2.jpeg', isFav: false},
+                {title: 'the final empire', author: 'brandon sanderson', img: 'assets/3.jpeg', isFav: true}
+            ],
+            url: "https://raiesbo.github.io/"
         }
     },
     methods: {
@@ -18,6 +26,28 @@ const app = Vue.createApp({
         },
         toggleShowBooks() {
             this.showBooks = !this.showBooks;
+        },
+        handleEvent(event, data) {
+            console.log(event)
+            console.log(event.type);
+
+            if (data) console.log(data);
+        },
+        handleMousemove(event) {
+            this.x = event.offsetX
+            this.y = event.offsetY
+        },
+        clickHandler(title) {
+            this.books.map((book, i) =>{
+                if (book.title === title) {
+                    this.books[i].isFav = !this.books[i].isFav
+                }}
+            );
+        },
+    },
+    computed: {
+        filteredBooks() {
+            return this.books.filter(book => book.isFav)
         }
     }
 });

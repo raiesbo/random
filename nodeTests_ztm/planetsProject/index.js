6378cc5f3ce1ac8aa2731e1a6df6516dd4cd58d4
raiesbo <raiesbo@gmail.com>
2022-07-01 22:45:23 +0200

    Finish planets project

--- nodeTests_ztm/planetsProject/index.js
+++ nodeTests_ztm/planetsProject/index.js
@@ -5,19 +5,31 @@ const results = [];
 
 const file = 'kepler_data.csv';
 
+const isHabitablePlanet = (planet) => {
+    return planet['koi_disposition'] === 'CONFIRMED'
+        && planet['koi_insol'] > 0.36
+        && planet['koi_insol'] < 1.11
+        && planet ['koi_prad'] < 1.6;
+}
+
 fs.createReadStream(file)
     .pipe(parse({
         comment: '#',
         columns: true
     }))
     .on('data', (data) => {
-        results.push(data);
+        if (isHabitablePlanet(data)) {
+            results.push(data);
+        }
     })
     .on('error', (error) => {
         console.log(error)
     })
     .on('end', () => {
-        console.log(results);
-        console.log('end of stream')
+        console.log(results.map(planet => {
+            return planet['kepler_name'];
+        }));
+        console.log(`Our program has found ${results.length} habitable  planets!`);
+        // console.log('End of stream.')
     })
     

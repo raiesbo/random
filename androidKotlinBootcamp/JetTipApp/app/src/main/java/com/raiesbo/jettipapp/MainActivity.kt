8f38455e3077b6ef0d0de6e153f06c3f21652288
raiesbo <raiesbo8@gmail.com>
2022-07-18 17:29:12 +0200

    [kotlinCompose JetTipApp] Refactor components and implement input element in view

--- androidKotlinBootcamp/JetTipApp/app/src/main/java/com/raiesbo/jettipapp/MainActivity.kt
+++ androidKotlinBootcamp/JetTipApp/app/src/main/java/com/raiesbo/jettipapp/MainActivity.kt
@@ -1,25 +1,30 @@
 package com.raiesbo.jettipapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.raiesbo.jettipapp.components.InputField
 import com.raiesbo.jettipapp.ui.theme.JetTipAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -27,7 +32,10 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyApp {
-                TopHeader()
+                Column() {
+                    TopHeader()
+                    MainContent()
+                }
             }
         }
     }
@@ -45,21 +53,12 @@ fun MyApp(content: @Composable () -> Unit) {
     }
 }
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Preview(showBackground = true)
 @Composable
 fun MainContent() {
-    Surface(
-        modifier = Modifier
-            .padding(2.dp)
-            .fillMaxWidth(),
-        shape = RoundedCornerShape(corner = CornerSize(12.dp)),
-        border = BorderStroke(width = 1.dp, color = Color.LightGray)
-    ) {
-        Column() {
-            Text(text = "test")
-            Text(text = "test")
-            Text(text = "test")
-        }
+    BillForm() { billAmt ->
+        Log.d("TAG", "Things happening here: $billAmt")
     }
 }
 
@@ -94,12 +93,54 @@ fun TopHeader(totalPerPerson: Double = 134.0) {
 
 // {}
 
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun BillForm(
+    modifier: Modifier = Modifier,
+    onValChange: (String) -> Unit = {}
+) {
+    val totalBillState = remember {
+        mutableStateOf("")
+    }
+
+    val validState = remember(totalBillState.value) {
+        totalBillState.value.trim().isNotEmpty()
+    }
+
+    val keyboardController = LocalSoftwareKeyboardController.current
+
+    Surface(
+        modifier = Modifier
+            .padding(2.dp)
+            .fillMaxWidth(),
+        shape = RoundedCornerShape(corner = CornerSize(12.dp)),
+        border = BorderStroke(width = 1.dp, color = Color.LightGray)
+    ) {
+        Column() {
+            InputField(
+                valueState = totalBillState,
+                labelId = "Enter Bill",
+                enabled = true,
+                isSingleLine = true,
+                onActions = KeyboardActions {
+                    if (!validState) return@KeyboardActions
+                    onValChange(totalBillState.value.trim())
+                    keyboardController?.hide()
+                }
+            )
+        }
+    }
+}
+
 //@Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     JetTipAppTheme {
         MyApp {
+            Column() {
                 TopHeader()
+                MainContent()
+            }
         }
     }
 }

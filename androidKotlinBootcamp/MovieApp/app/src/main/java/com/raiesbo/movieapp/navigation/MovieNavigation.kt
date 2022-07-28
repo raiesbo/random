18c5ac49c89caeaf59a83cc3c271cb1febc510c9
raiesbo <raiesbo@gmail.com>
2022-07-28 13:29:12 +0200

    [kotlinCompose MovieApp] Finish nav implementation

--- androidKotlinBootcamp/MovieApp/app/src/main/java/com/raiesbo/movieapp/navigation/MovieNavigation.kt
+++ androidKotlinBootcamp/MovieApp/app/src/main/java/com/raiesbo/movieapp/navigation/MovieNavigation.kt
@@ -1,17 +1,30 @@
 package com.raiesbo.movieapp.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.raiesbo.movieapp.screens.details.DetailsScreen
+import com.raiesbo.movieapp.screens.home.HomeScreen
 
 @Composable
 fun MovieNavigation() {
     val navController = rememberNavController()
     NavHost(navController = navController, startDestination = MovieScreens.HomeScreen.name) {
-        // Here come the composables that for the nav graph
         composable(MovieScreens.HomeScreen.name) {
-
+            // Here we pass where this should lead us to
+            HomeScreen(navController = navController)
+        }
+        composable(
+            route = MovieScreens.DetailsScreen.name+"/{movie}",
+            arguments = listOf(navArgument(name = "movie"){type = NavType.StringType})
+        ) { backStackEntry ->
+            DetailsScreen(
+                navController = navController,
+                movieData = backStackEntry.arguments?.getString("movie")
+            )
         }
     }
 }
---
+++ androidKotlinBootcamp/MovieApp/app/src/main/java/com/raiesbo/movieapp/screens/details/DetailsScreen.kt
@@ -0,0 +1,51 @@
+package com.raiesbo.movieapp.screens.details
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+
+@Composable
+fun DetailsScreen(navController: NavController, movieData: String?) {
+    Scaffold(
+        topBar = {
+            TopAppBar(backgroundColor = Color.LightGray, elevation = 5.dp) {
+                Row(horizontalArrangement = Arrangement.Start, verticalAlignment = Alignment.CenterVertically) {
+                    Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "Arrow Back", modifier = Modifier.clickable {
+                        navController.popBackStack()
+                    })
+                    Spacer(modifier = Modifier.width(100.dp))
+                    Text(text = "Movies")
+                }
+            }
+        }
+    ) {
+        Surface(modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(text = movieData.toString(), style = MaterialTheme.typography.h5)
+                Spacer(modifier = Modifier.height(20.dp))
+                Button(onClick = {
+                    navController.popBackStack()
+                }) {
+                    Text(text = "Go Back")
+                }
+            }
+        }
+    }
+
+
+
+
+}

3ace780170064cd52bf642961f388b6641eb14ce
raiesbo <raiesbo@gmail.com>
2022-07-30 12:38:31 +0200

    [kotlinCompose MovieApp] Implement all basic functionalities

---
+++ androidKotlinBootcamp/JetNoteApp/app/src/main/java/com/raiesbo/jetnoteapp/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.raiesbo.jetnoteapp
+
+import android.os.Build
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.raiesbo.jetnoteapp.data.NoteDataSource
+import com.raiesbo.jetnoteapp.model.Note
+import com.raiesbo.jetnoteapp.screen.NoteScreen
+import com.raiesbo.jetnoteapp.ui.theme.JetNoteAppTheme
+
+class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetNoteAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    val notes = remember {
+                        mutableStateListOf<Note>()
+                    }
+
+                    NoteScreen(
+                        notes = notes,
+                        onAddNote = {
+                            notes.add(it)
+                        },
+                        onRemoveNote = {
+                            notes.remove(it)
+                        }
+                    )
+                }
+            }
+        }
+    }
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    JetNoteAppTheme {
+        NoteScreen(
+            notes = NoteDataSource().loadNotes(),
+            onAddNote = {},
+            onRemoveNote = {}
+        )
+    }
+}

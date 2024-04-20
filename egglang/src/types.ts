6090e2d3420c9828eaca953bf3725a9685e1c9b8
raiesbo <raiesbo@gmail.com>
2024-04-20 14:59:42 +0200

    implement egglang parser

---
+++ egglang/src/types.ts
@@ -0,0 +1,10 @@
+export type Expression = {
+    type: string,
+    value?: Value,
+    name?: string,
+    operator?: Expression,
+    args?: Array<Expression>,
+    rest?: string
+}
+
+export type Value = string | number

6250b7acf11871441e1bd34ccaa2bbd004a8223e
raiesbo <raiesbo@gmail.com>
2024-05-01 15:11:40 +0200

    finish egglang implementation

--- egglang/src/main.ts
+++ egglang/src/main.ts
@@ -1,5 +1,6 @@
 import { Expression, Scope } from "./types";
 
+
 function parseExpression(program: string) {
 	program = skipSpace(program);
 	let match, expr;
@@ -124,4 +125,65 @@ specialForms.define = (args: Array<Expression>, scope: Scope) => {
 	return value
 }
 
-console.log(parse("+(a, 10)"))
+specialForms.fun = (args: Array<Expression>, scope: Scope) => {
+	if (!args.length) {
+		throw new SyntaxError("Functions need a body")
+	}
+	let body = args[args.length - 1]
+	let params = args.slice(0, args.length - 1).map(expr => {
+		if (expr.type != "word") {
+			throw new SyntaxError("Parameter names must be words")
+		}
+		return expr.name
+	})
+
+	return function () {
+		if (arguments.length != params.length) {
+			throw new TypeError("Wrong number of arguments")
+		}
+		let localScope = Object.create(scope)
+		for (let i = 0; i < arguments.length; i++) {
+			localScope[params[i] as string] = arguments[i]
+		}
+		return evaluate(body, localScope)
+	}
+}
+
+const topScope = Object.create(null)
+topScope.true = true
+topScope.false = false
+
+for (let op of ["+", "-", "*", "/", "==", "<", ">"]) {
+	topScope[op] = Function("a, b", `return a ${op} b;`)
+}
+
+topScope.print = (value: any) => {
+	console.log(value)
+	return value
+}
+
+export default function run(program: string) {
+	return evaluate(parse(program), Object.create(topScope))
+}
+
+run(`
+do(define(total, 0),
+	define(count, 1),
+	while(<(count, 11),
+		do(define(total, +(total, count)),
+			define(count, +(count, 1)))),
+	print(total))
+`) // 55
+
+run(`
+do(define(plusOne, fun(a, +(a,1))),
+	print(plusOne(10)))
+`) // 11
+
+run(`
+do(define(pow, fun(base, exp,
+	if(==(exp, 0),
+		1,
+		*(base, pow(base, -(exp, 1)))))),
+	print(pow(2, 10)))
+`) // 1024

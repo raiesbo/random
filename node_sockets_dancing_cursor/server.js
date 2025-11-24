b9c62ea62becbea6199bfc94d49e88c513d1a8cc
raiesbo <raiesbo@gmail.com>
2025-11-24 20:49:41 +0100

    extend WebSocket server: add client metadata, message broadcasting, and UUID-based identification

--- node_sockets_dancing_cursor/server.js
+++ node_sockets_dancing_cursor/server.js
@@ -1,12 +1,15 @@
-const express = require('express');
-const path = require('path');
-const {createServer} = require('http');
-const {WebSocketServer} = require('ws');
+import express from 'express';
+import path from 'path';
+import {createServer} from 'http';
+import {WebSocketServer} from 'ws';
+import {v4} from "uuid";
 
 const PORT = process.env.PORT || 3000;
 
+const clients = new Map();
+
 const app = express();
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static('public'));
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
 
@@ -14,9 +17,32 @@ const server = createServer(app);
 const wss = new WebSocketServer({server});
 
 wss.on('connection', function (ws) {
+    const id = v4();
+    const color = Math.floor(Math.random() * 360);
+    const metadata = {id, color};
+
+    clients.set(ws, metadata);
+
+    ws.on('message', (messageString) => {
+        const message = JSON.parse(messageString);
+        const metadata = clients.get(ws);
+
+        message.sender = metadata.id
+        message.color = metadata.color;
+
+        const outbound = JSON.stringify({
+            ...message,
+            sender: metadata.id,
+            color: metadata.color,
+        });
+
+        [...clients.keys()].forEach(client => client.send(outbound));
+    })
+
     ws.on('error', console.error);
 
-    ws.on('close', function () {
+    ws.on('close', () => {
+        clients.delete(ws);
         console.log('stopping client interval');
     });
 });

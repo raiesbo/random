aaac8eb705f3491ab91f2cdc4bba91225c299c46
raiesbo <raiesbo@gmail.com>
2025-11-28 00:56:52 +0100

    extend WebSocket server: integrate Redis for message broadcasting, replace in-memory client handling with Redis pub/sub

--- node-sockets-dancing-cursor/server.js
+++ node-sockets-dancing-cursor/server.js
@@ -3,14 +3,34 @@ import path from 'path';
 import {createServer} from 'http';
 import {WebSocketServer} from 'ws';
 import {v4} from "uuid";
+import {createClient} from "redis";
 
 const PORT = process.env.PORT || 3000;
 const dir = import.meta.dirname;
 
-const clients = new Map();
+const wsClients = new Map();
 
+// Express App
 const app = express();
 app.use(express.static(path.join(dir, 'public')));
+
+// Redis Clients
+const redisSubscriber = createClient();
+const redisPublisher = createClient();
+
+await redisSubscriber
+    .on('error', err => console.error("Redis Subscriber Error:", err))
+    .connect();
+
+await redisPublisher
+    .on('error', err => console.error("Redis Publisher Error:", err))
+    .connect();
+
+await redisSubscriber.subscribe('message', (message) => {
+    [...wsClients.keys()].forEach(client => {
+        client.readyState === WebSocket.OPEN && client.send(message);
+    });
+}, false);
 
 app.get('/', (req, res) => res.sendFile(path.join(dir, '/public/index.html')));
 
@@ -22,32 +42,30 @@ wss.on('connection', (ws) => {
     const color = Math.floor(Math.random() * 360);
     const metadata = {id, color};
 
-    clients.set(ws, metadata);
+    wsClients.set(ws, metadata);
 
-    ws.on('message', (messageString) => {
+    ws.on('message', async (messageString) => {
         const message = JSON.parse(messageString);
-        const metadata = clients.get(ws);
+        const metadata = wsClients.get(ws);
 
         message.sender = metadata.id
         message.color = metadata.color;
 
-        const outbound = JSON.stringify({
-            ...message,
-            sender: metadata.id,
-            color: metadata.color,
-        });
+        const outbound = JSON.stringify(message);
+        await redisPublisher.publish('message', outbound);
 
-        [...clients.keys()].forEach(client => client.send(outbound));
+
+        // [...wsClients.keys()].forEach(client => client.send(outbound));
     })
 
     ws.on('error', console.error);
 
     ws.on('close', () => {
-        clients.delete(ws);
+        wsClients.delete(ws);
         console.log('stopping client interval');
     });
 });
 
-server.listen(PORT, () => {
+server.listen(PORT, async () => {
     console.log(`Listening on http://localhost:${PORT}`);
 });

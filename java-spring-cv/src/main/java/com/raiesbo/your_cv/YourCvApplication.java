fb179a4f9a4a7bc9054cad8ba4598ae0eafaea7c
raiesbo <raiesbo@gmail.com>
2026-01-30 19:23:12 +0100

    spring-boot-cv first commit with framework boilerplate

---
+++ java-spring-cv/src/main/java/com/raiesbo/your_cv/YourCvApplication.java
@@ -0,0 +1,13 @@
+package com.raiesbo.your_cv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourCvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourCvApplication.class, args);
+	}
+
+}
---
+++ java-spring-cv/src/test/java/com/raiesbo/your_cv/YourCvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raiesbo.your_cv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourCvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
